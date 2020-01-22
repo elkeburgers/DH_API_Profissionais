397042c62d307b9fa3cf5ac3764af6d790e11ff9
Elke Burgers <elke.burgers@hotmail.com>
2020-01-22 12:04:18 -0300

    criacao de metodo para teste e de frontend para cadastrar dados e retornar resultados. Falta liberar o CORS e consumor API.

--- backend/app/Http/Controllers/ProfissionalController.php
+++ backend/app/Http/Controllers/ProfissionalController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Profissionais;
+use App\Profissional;
 
 class ProfissionalController extends Controller
 {
@@ -13,5 +13,18 @@ class ProfissionalController extends Controller
 
         // nao retorna view por ser uma API, apenas um json, que precisa ser uma resposta (response)(palavra reservada do laravel):
         return response()->json($listaProfissionais);
+    }
+
+    // para criar os profissionais:
+    public function criarProfissional(Request $request){
+        $newProfissional = new Profissional();
+        // dizendo a coluna da tabela onde estah o dado que estamos criando, nomes iguais aos das migrations:
+        $newProfissional->name = $request->name;
+        $newProfissional->github = $request->github;
+        // usa o result para salvar tudo dentro de um lugar:
+        $result = $newProfissional->save();
+
+        // para saber se deu certo, retornando na tela apenas os dados do profissional criado. Se usasse result, retornaria True ou False:
+        return response()->json($newProfissional);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/profissionais', 'ProfissionalController@listarProfissionais');
+Route::post('/profissonais', 'ProfissionalCOntroller@criarProfissional');

0e80685584385e48f14b3bac3f48d36621f0a8cd
Elke Burgers <elke.burgers@hotmail.com>
2020-01-24 12:09:02 -0300

    adicao de tecnologia ao profissonal no controller. Precisa fazer o html.

--- backend/app/Http/Controllers/ProfissionalController.php
+++ backend/app/Http/Controllers/ProfissionalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Profissional;
+use App\Tecnologia;
 
 class ProfissionalController extends Controller
 {
@@ -17,12 +18,23 @@ class ProfissionalController extends Controller
 
     // para criar os profissionais:
     public function criarProfissional(Request $request){
+        // acrescentando tecnologia para associar ao profissional:
+        $tecnologiaId = $request->tecnologia;
         $newProfissional = new Profissional();
         // dizendo a coluna da tabela onde estah o dado que estamos criando, nomes iguais aos das migrations:
         $newProfissional->name = $request->name;
         $newProfissional->github = $request->github;
         // usa o result para salvar tudo dentro de um lugar:
         $result = $newProfissional->save();
+
+        // acrescentando tecnologia para associar ao profissional:
+        // dd($tecnologiaId); -> apenas para imprimir na tela do Postman
+        $tecnologia = Tecnologia::find($tecnologiaId);
+        if($tecnologia){
+            $tecnologia->profissionais()->attach($newProfissional->id);
+        }else{
+            return response()->json(["error"=>"O id da tecnologia não existe."]);
+        }
 
         // para saber se deu certo, retornando na tela apenas os dados do profissional criado. Se usasse result, retornaria True ou False:
         return response()->json($newProfissional);

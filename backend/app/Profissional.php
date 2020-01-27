3ee8e6e0db197a057066c0e2892044bf3b4c300b
Elke Burgers <elke.burgers@hotmail.com>
2020-01-27 08:48:34 -0300

    ccorreçao do codigo para adequar a relacao de tabela N:N

--- backend/app/Profissional.php
+++ backend/app/Profissional.php
@@ -11,7 +11,8 @@ class Profissional extends Model
     //criando as relacoes entre as tabelas:
     public function tecnologias(){
         // hasMany tem quase o mesmo trabalho do belongsToMany, muda a referencia (eles TEM a tecnologia, no outro models a tecnologia PERTENCE ao profissional):
-        return $this->hasMany('App\Tecnologia', 'profissionais_tecnologias', 'profissional_id', 'tecnologia_id');
+        // o Laravel, sempre que a tabela  eh N para N, tem que ser belongsToMany de ambos os lados:
+        return $this->belongsToMany('App\Tecnologia', 'profissionais_tecnologias', 'profissional_id', 'tecnologia_id');
     }
 }
 
